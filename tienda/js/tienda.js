81a060ed3000e961a249c296690a43bcf6076451
Daniel Castaño <danielcast244@gmail.com>
2022-04-28 11:15:17 -0500

    PROYECTO DE ECOMMERCE DE BOOKSTORE SE CREO PAGINA DE INICIO, DETALLE PRODUCTO, CARRITO, QUIENES SOMOS, CONTACTOS Y LOGIN

---
+++ tienda/js/tienda.js
@@ -0,0 +1,24 @@
+//----------------------CONSTANTES ---------------------- //
+const añadirAlCarrito = document.querySelectorAll('.añadir')
+
+añadirAlCarrito.forEach(añadirCarrito => {
+    añadirCarrito.addEventListener('click', añadirLibro)
+})
+
+function añadirLibro(event) {
+    const button = event.target
+    const Card = button.closest('.Card')
+
+    const bookName = Card.querySelector('.Card-Name').textContent
+    const bookPrice = Card.querySelector('.Card-Price').textContent
+    const bookAutor = Card.querySelector('.Card-Autor').textContent
+    const bookImg = Card.querySelector('.imgCard').src
+
+    console.log(bookName, bookPrice, bookAutor, bookImg)
+}
+
+// /*TIENDA*/
+// window.addEventListener("scroll", function(){
+//     var header = document.querySelector("header");
+// 		header.classList.toggle("abajo",window.scrollY>0);
+// })
